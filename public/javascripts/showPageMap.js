9f7105f5d49fbda3d15dae273912ff18ec13ac2b
Yash <yash.aanand06@gmail.com>
2025-07-16 14:40:26 +0530

    maptiler

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,21 +1,17 @@
-mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
-    center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
+maptilersdk.config.apiKey = maptilerApiKey;
+
+const map = new maptilersdk.Map({
+  container: "map",
+  style: maptilersdk.MapStyle.BRIGHT,
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 10, // starting zoom
 });
 
-map.addControl(new mapboxgl.NavigationControl());
-
-
-new mapboxgl.Marker()
-    .setLngLat(campground.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-                `<h3>${campground.title}</h3><p>${campground.location}</p>`
-            )
+new maptilersdk.Marker()
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
     )
-    .addTo(map)
-
+  )
+  .addTo(map);
